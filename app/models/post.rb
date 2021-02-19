f65c05a99a0180c78d5bca7481a05b6e967eadda
oxenprogrammer <emmy1000okello@gmail.com>
2021-02-20 01:47:58 +0300

    feat(reddit): add post title validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comment
+  validates :title, uniqueness: true
+  validates :title, length: {maximum: 50}
+  validates :title, length: {mimmum: 50}
 end
