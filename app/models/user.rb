77f15dcadf45fe552e24c1fdbb482bcfed789771
unknown <paulclue20@gmail.com>
2021-02-22 17:46:13 -0500

    update validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :post
   has_many :comment
-  validates :name, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :name, presence: true, length: { minimum: 4, maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { minimum: 4, maximum: 30 }
 end
