153c58dbcabfd85c716e3e755a281c15e454728f
oxenprogrammer <emmy1000okello@gmail.com>
2021-02-23 01:32:20 +0300

    style(linter): hotfix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates :body, length: {minimum: 50, maximum: 250}, presence: true
+  validates :body, length: { minimum: 50, maximum: 250 }, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comment
-  validates :title, uniqueness: true, length: {minimum: 6, maximum: 50}
-  validates :body, length: {minimum: 50, maximum: 250}
+  validates :title, uniqueness: true, length: { minimum: 6, maximum: 50 }
+  validates :body, length: { minimum: 50, maximum: 250 }
 end
