d7666bab3e8875e923b5f8e82f6818f8d5971f7e
unknown <paulclue20@gmail.com>
2021-02-22 17:46:53 -0500

    update validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comment
-  validates :title, uniqueness: true, length: { minimum: 6, maximum: 50 }
+  validates :title, uniqueness: true, length: { minimum: 6, maximum: 30 }
   validates :body, length: { minimum: 50, maximum: 300 }
 end
